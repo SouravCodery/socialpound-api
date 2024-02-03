1b1ce7f8041dc7ba5d68121a2590bc3f83b6317c
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-02-03 15:15:03 +0530

    x-powered-by disabled and express.json() addition in app.js
    app.disable("x-powered-by") and app.use(express.json())

--- app.ts
+++ app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import router from "./src/routes/";
 
 const app = express();
+
+app.disable("x-powered-by");
+
+app.use(express.json());
 app.use("/", router);
 
 export default app;
