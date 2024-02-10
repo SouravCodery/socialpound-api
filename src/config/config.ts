778b214152dd80e168453014c0ad9c65ae7a82a9
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-02-10 14:33:16 +0530

    config setup

---
+++ src/config/config.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+
+interface Config {
+  MONGODB_URI: string;
+}
+
+const getEnvVariable = (key: string, defaultValue?: string): string => {
+  const value = process.env[key] || defaultValue;
+
+  if (value === undefined) {
+    throw new Error(`Environment variable ${key} is not set!`);
+  }
+
+  return value;
+};
+
+const config: Readonly<Config> = Object.freeze({
+  MONGODB_URI: getEnvVariable("MONGODB_URI"),
+});
+
+export default config;
