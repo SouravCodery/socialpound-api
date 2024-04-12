58a42f005bd526f993f545ea22e43f0431a5c0ad
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-04-12 18:37:59 +0530

    feat: cors package applied

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./src/routes/";
+import cors from "cors";
 
 const app = express();
 
 app.disable("x-powered-by");
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
