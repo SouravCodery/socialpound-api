7346bd4be3cb463da11593d5d5a0306026dfb735
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-04-13 18:13:49 +0530

    feat: error handling middleware changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import v1Router from "./v1/";
+import { HttpError } from "../classes/http-error.class";
 
 const router = express.Router();
 
@@ -10,7 +11,19 @@ router.get("/", (req, res) => {
 router.use("/v1", v1Router);
 
 router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).send("Internal Server Error");
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      error: {
+        message: err.message,
+      },
+    });
+  }
+
+  return res.status(500).json({
+    error: {
+      message: "Internal Server Error",
+    },
+  });
 });
 
 export default router;
