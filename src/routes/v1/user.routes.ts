e1b6cdce61383e808ab0b8ebfb060eba78d0b5d1
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-04-13 18:14:59 +0530

    feat: authMiddleware integration

--- src/routes/v1/user.routes.ts
+++ src/routes/v1/user.routes.ts
@@ -3,12 +3,14 @@ import express from "express";
 import * as userController from "../../controllers/v1/user.controllers";
 import * as userValidationSchemas from "../../schemas/user-routes.validators";
 
-import { validate } from "../../helpers/joi.helper";
+import { validate } from "../../middlewares/validate.middleware";
+import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const userRouter = express.Router();
 
 userRouter.post(
   "/sign-in",
+  authMiddleware,
   validate(userValidationSchemas.signInSchema),
   userController.signIn
 );
