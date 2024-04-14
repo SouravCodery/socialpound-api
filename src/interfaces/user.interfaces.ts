2fdeb44d37fc702f9926af6b16153e96526bbb2f
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-04-14 19:36:45 +0530

    feat: UserInterface updated
    feat: UserInterface updated with GoogleAuthUserInterface, GitHubAuthUserInterface

--- src/interfaces/user.interfaces.ts
+++ src/interfaces/user.interfaces.ts
@@ -1,41 +1,7 @@
 import { Document } from "mongoose";
 
-export interface OAuthUserInterface {
-  id: string;
-  name: string;
-  email: string;
-  image: string;
-}
-
-export interface GoogleAuthInterface {
-  user: OAuthUserInterface;
-  account: {
-    access_token: string;
-    expires_in: number;
-    scope: string;
-    token_type: string;
-    id_token: string;
-    expires_at: number;
-    provider: "google";
-    type: string;
-    providerAccountId: string;
-  };
-  profile: {
-    iss: string;
-    azp: string;
-    aud: string;
-    sub: string;
-    email: string;
-    email_verified: boolean;
-    at_hash: string;
-    name: string;
-    picture: string;
-    given_name: string;
-    family_name: string;
-    iat: number;
-    exp: number;
-  };
-}
+import { GoogleAuthUserInterface } from "./google-auth-user.interface";
+import { GitHubAuthUserInterface } from "./github-auth-user.interface";
 
 export interface UserInterface {
   username: string;
@@ -46,16 +12,11 @@ export interface UserInterface {
   postsCount?: number;
   followersCount?: number;
   followingCount?: number;
-  googleAuth?: GoogleAuthInterface;
+  googleAuthUser?: GoogleAuthUserInterface;
+  githubAuthUser?: GitHubAuthUserInterface;
   isPrivate: boolean;
   isDeleted: boolean;
   deletedAt?: Date;
 }
 
-export interface GoogleAuthDocumentInterface
-  extends Document,
-    GoogleAuthInterface {}
-
-export interface UserDocumentInterface extends Document, UserInterface {
-  googleAuth?: GoogleAuthDocumentInterface;
-}
+export interface UserDocumentInterface extends Document, UserInterface {}
