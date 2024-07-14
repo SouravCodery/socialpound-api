4a86879bd9c189f537d0540aa5c3efad35099696
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-07-14 14:22:41 +0530

    feat: CommentModel addition

---
+++ src/models/comment.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+import baseSchemaOptions from "./base-schema-options";
+
+import { CommentDocumentInterface } from "./../interfaces/comment.interface";
+
+const commentSchema: Schema<CommentDocumentInterface> = new Schema(
+  {
+    commentOn: { type: String, enum: ["Post", "Comment"], required: true },
+
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    parentComment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
+
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+    text: { type: String, maxLength: 2200, required: true },
+
+    likesCount: { type: Number, default: 0, required: true },
+    repliesCount: { type: Number, default: 0, required: true },
+
+    isDeleted: { type: Boolean, default: false, required: true },
+    deletedAt: { type: Date, default: null },
+  },
+  baseSchemaOptions
+);
+
+commentSchema.pre("validate", function (next) {
+  if (this.commentOn === "Comment" && !this.parentComment) {
+    next(new Error(`parentComment is required when likeOn is 'Comment'`));
+  } else {
+    next();
+  }
+});
+
+const Comment = model("Comment", commentSchema);
+export default Comment;
