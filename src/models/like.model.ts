a156ab2a3f1fc4db6d269388de04454646893422
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-07-14 13:51:28 +0530

    feat: LikeModel addition

---
+++ src/models/like.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+import baseSchemaOptions from "./base-schema-options";
+
+import { LikeDocumentInterface } from "./../interfaces/like.interface";
+
+const likeSchema: Schema<LikeDocumentInterface> = new Schema(
+  {
+    likeOn: { type: String, enum: ["Post", "Comment"], required: true },
+
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    comment: { type: Schema.Types.ObjectId, ref: "Comment", default: null },
+
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+    isDeleted: { type: Boolean, default: false },
+    deletedAt: { type: Date },
+  },
+  baseSchemaOptions
+);
+
+likeSchema.pre("validate", function (next) {
+  if (this.likeOn === "Comment" && !this.comment) {
+    next(new Error(`comment is required when likeOn is 'Comment'`));
+  } else {
+    next();
+  }
+});
+
+const Like = model("Like", likeSchema);
+export default Like;
