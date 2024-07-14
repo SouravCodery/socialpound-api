661780b6a92ca1228210b06e58de043774c32be3
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-07-14 15:05:16 +0530

    feat: Change in UserInterface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -6,21 +6,22 @@ import { GitHubAuthUserInterface } from "./github-auth-user.interface";
 export interface UserInterface {
   username: string;
   email: string;
-  fullName?: string;
+  fullName: string;
+
+  profilePicture: string;
+  bio: string;
+
+  postsCount: number;
+  followersCount: number;
+  followingCount: number;
+
+  googleAuthUser: GoogleAuthUserInterface | null;
+  githubAuthUser: GitHubAuthUserInterface | null;
+
   isPrivate: boolean;
 
-  profilePicture?: string;
-  bio?: string;
-
-  postsCount?: number;
-  followersCount?: number;
-  followingCount?: number;
-
-  googleAuthUser?: GoogleAuthUserInterface;
-  githubAuthUser?: GitHubAuthUserInterface;
-
   isDeleted: boolean;
-  deletedAt?: Date;
+  deletedAt: Date | null;
 }
 
 export interface UserDocumentInterface extends Document, UserInterface {}
