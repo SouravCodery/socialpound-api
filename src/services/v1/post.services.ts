2e115e1f5510dd061da27f59692b75ad4ffd41ac
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-07-21 02:02:38 +0530

    feat: Add functional createPost service

--- src/services/v1/post.services.ts
+++ src/services/v1/post.services.ts
@@ -1,13 +1,34 @@
+import mongoose from "mongoose";
+
 import { HttpError } from "../../classes/http-error.class";
 import { HttpResponse } from "../../classes/http-response.class";
 
 import { logger } from "../../logger/index.logger";
+import Post from "../../models/post.model";
 
-export const createPost = async ({}: {}) => {
+import { PostInterface } from "./../../interfaces/post.interface";
+
+export const createPost = async ({
+  user,
+  content,
+  caption,
+}: {
+  user: PostInterface["user"];
+  content: PostInterface["content"];
+  caption: PostInterface["caption"];
+}) => {
   try {
+    const newPost = new Post({
+      user,
+      content,
+      caption,
+    });
+
+    await newPost.save();
+
     return new HttpResponse({
-      status: 200,
-      message: "Post creation Successful",
+      status: 201,
+      message: "Post created successfully",
     });
   } catch (error) {
     logger.error("[Service: createPost] - Something went wrong", error);
