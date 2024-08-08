980f2136f87ad41a52b457ef348aa046b252af64
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-08 20:31:25 +0530

    feat: Change Err Middleware and Add 404 Handler
    Change message in Error Handler middleware
    Add 404 Route Handler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
+
 import v1Router from "./v1/";
 import { HttpError } from "../classes/http-error.class";
+import { logger } from "../logger/index.logger";
 
 const router = express.Router();
 
@@ -13,16 +15,20 @@ router.use("/v1", v1Router);
 router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof HttpError) {
     return res.status(err.status).json({
-      error: {
-        message: err.message,
-      },
+      message: err.message,
     });
   }
 
+  logger.error("[Error handling middleware] - Unhandled error", err);
+
   return res.status(500).json({
-    error: {
-      message: "Internal Server Error",
-    },
+    message: "Internal Server Error",
+  });
+});
+
+router.use((req: Request, res: Response) => {
+  return res.status(404).json({
+    message: "Invalid Route",
   });
 });
 
