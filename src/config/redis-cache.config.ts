fa16b742cb6893b795352d04502233971cae8ec9
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-10 17:43:15 +0530

    feat: Add Cache and Persistent Redis Client

---
+++ src/config/redis-cache.config.ts
@@ -0,0 +1,20 @@
+// Cache Redis instance on port 6380
+
+// docker run -d \
+//   --name redis-cache \
+//   -p 6380:6379 \
+//   redis:latest \
+//   redis-server --appendonly no --save "" --maxmemory-policy allkeys-lru
+
+import { createClient } from "redis";
+import { Config } from "./config";
+
+import { logger } from "../logger/index.logger";
+
+export const cacheRedisClient = createClient({
+  url: Config.REDIS_CACHE_URL,
+});
+
+cacheRedisClient.on("error", (err) =>
+  logger.error("Cache Redis Client Error", err)
+);
