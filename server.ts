1e7724ea80d028b52a5217915d021524767fcf00
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-10 17:43:47 +0530

    feat: Add Redis Clients connection in server.ts

--- server.ts
+++ server.ts
@@ -2,12 +2,29 @@ import app from "./app";
 import connectToDatabase from "./src/config/database.config";
 import { Config } from "./src/config/config";
 
+import { persistentRedisClient } from "./src/config/redis-persistent.config";
+import { cacheRedisClient } from "./src/config/redis-cache.config";
+
 import { logger } from "./src/logger/index.logger";
 
 const port = Config.PORT;
 
-connectToDatabase().then(() => {
-  app.listen(port, () => {
-    logger.info(`Socialpound API listening on: ${port}`);
-  });
-});
+(async () => {
+  try {
+    await Promise.all([
+      connectToDatabase(),
+      persistentRedisClient.connect(),
+      cacheRedisClient.connect(),
+    ]);
+
+    logger.info("Connected to persistentRedis and cacheRedis");
+
+    app.listen(port, () => {
+      logger.info(`Socialpound API listening on: ${port}`);
+    });
+  } catch (error) {
+    logger.error("Error during server setup", error);
+
+    process.exit(1);
+  }
+})();
