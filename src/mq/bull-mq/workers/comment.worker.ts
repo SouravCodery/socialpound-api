c50a136170571a050259311227d6a3ab13fe9159
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-11 18:38:27 +0530

    feat: Integrate addComment service in Comment Worker

--- src/mq/bull-mq/workers/comment.worker.ts
+++ src/mq/bull-mq/workers/comment.worker.ts
@@ -1,10 +1,33 @@
 import { Worker } from "bullmq";
+
 import { bullMQConnection } from "../../../config/bull-mq.config";
+import { addComment } from "../../../services/v1/comment.services";
+
+import { logger } from "../../../logger/index.logger";
 
 export const commentWorker = new Worker(
   "comment",
   async (job) => {
-    console.log(job.id);
+    try {
+      const jobName = job.name;
+
+      if (jobName !== "add-comment") {
+        return;
+      }
+
+      const { commentOn, post, parentComment, user, text } = job.data;
+
+      await addComment({
+        commentOn,
+        post,
+        parentComment,
+        user,
+        text,
+      });
+    } catch (error) {
+      logger.error("Error in comment worker", error);
+      throw error;
+    }
   },
   {
     connection: bullMQConnection,
@@ -12,9 +35,11 @@ export const commentWorker = new Worker(
 );
 
 commentWorker.on("completed", (job) => {
-  console.log(`${job.id} has completed!`);
+  logger.info(`[Worker: commentWorker] - ${job.name} ${job.id} has completed!`);
 });
 
 commentWorker.on("failed", (job, err) => {
-  console.log(`${job?.id} has failed with ${err.message}`);
+  logger.error(
+    `[Worker: commentWorker] - ${job?.name} ${job?.id} has failed with ${err.message}`
+  );
 });
