5882f6ecfbd58ff3c001e67ee1f10c7fcfb6c4a2
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-11 17:00:12 +0530

    feat: Add comment routes to index

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+
 import userRouter from "./user.routes";
 import postRouter from "./post.routes";
+import commentRouter from "./comment.routes";
 
 const v1Router = express.Router();
 
 v1Router.use("/user", userRouter);
 v1Router.use("/post", postRouter);
+v1Router.use("/comment", commentRouter);
 
 export default v1Router;
