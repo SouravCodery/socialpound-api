4ac93161133a6da71179b212d351aa71dc7e5ad3
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-11 17:53:55 +0530

    feat: Add validation in addComment for relationships

--- src/services/v1/comment.services.ts
+++ src/services/v1/comment.services.ts
@@ -4,6 +4,8 @@ import { HttpResponse } from "../../classes/http-response.class";
 import { logger } from "../../logger/index.logger";
 
 import Comment from "../../models/comment.model";
+import Post from "../../models/post.model";
+
 import { CommentInterface } from "./../../interfaces/comment.interface";
 
 export const addComment = async ({
@@ -24,6 +26,27 @@ export const addComment = async ({
   text: CommentInterface["text"];
 }) => {
   try {
+    const postExists = await Post.exists({ _id: post, isDeleted: false });
+
+    if (!postExists) {
+      throw new HttpError(404, "Post not found");
+    }
+
+    if (commentOn === "Comment") {
+      const parentCommentExists = await Comment.findOne({
+        _id: parentComment,
+        isDeleted: false,
+      }).select("commentOn");
+
+      if (!parentCommentExists) {
+        throw new HttpError(404, "Parent comment not found");
+      }
+
+      if (parentCommentExists.commentOn !== "Post") {
+        throw new HttpError(400, "Reply to a comment reply is not allowed");
+      }
+    }
+
     const newComment = new Comment({
       commentOn,
       post,
