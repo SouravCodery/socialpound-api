c0f5d522de0019a4455121fce64fcf079a0233dc
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-08-12 13:35:03 +0530

    feat: Add incrementLikesCommentCount and getLikesCommentCount

---
+++ src/services/v1/persistent-redis.services.ts
@@ -0,0 +1,41 @@
+import { persistentRedisClient } from "../../config/redis-persistent.config";
+import { logger } from "../../logger/index.logger";
+
+export const incrementLikesCommentCount = async ({
+  entity,
+  id,
+  countType,
+}: {
+  entity: "Post" | "Comment";
+  id: string;
+  countType: "likesCount" | "commentsCount";
+}) => {
+  try {
+    const hashKey = `${entity}:${id}`;
+    const result = await persistentRedisClient.hIncrBy(hashKey, countType, 1);
+
+    return result;
+  } catch (error) {
+    logger.error("[Service: incrementCount] - Something went wrong", error);
+  }
+};
+
+export const getLikesCommentCount = async ({
+  entity,
+  id,
+}: {
+  entity: "Post" | "Comment";
+  id: string;
+}) => {
+  try {
+    const hashKey = `${entity}:${id}`;
+    const result = await persistentRedisClient.hGetAll(hashKey);
+
+    return {
+      likesCount: Number(result.likesCount ?? 0),
+      commentsCount: Number(result.commentsCount ?? 0),
+    };
+  } catch (error) {
+    logger.error("[Service: getCount] - Something went wrong", error);
+  }
+};
