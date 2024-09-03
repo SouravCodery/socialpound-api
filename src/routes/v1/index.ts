5a3d33a707d4bbd1e1a95776e4b8285ad0d698f7
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-03 23:55:05 +0530

    feat: Add aws-presigned-url route

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,11 +3,13 @@ import express from "express";
 import userRouter from "./user.routes";
 import postRouter from "./post.routes";
 import commentRouter from "./comment.routes";
+import awsSignedUrlRouter from "./aws-presigned-url.routes";
 
 const v1Router = express.Router();
 
 v1Router.use("/user", userRouter);
 v1Router.use("/post", postRouter);
 v1Router.use("/comment", commentRouter);
+v1Router.use("/aws-presigned-url", awsSignedUrlRouter);
 
 export default v1Router;
