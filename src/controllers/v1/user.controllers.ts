fc8e65cfa4e67916af7c8ebd601a4a72f8dc5ee4
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-07 19:06:56 +0530

    feat: Add getUserByUsername

--- src/controllers/v1/user.controllers.ts
+++ src/controllers/v1/user.controllers.ts
@@ -1,11 +1,11 @@
+import { AuthenticatedRequestInterface } from "../../interfaces/extended-request.interface";
+
 import { Request, Response, NextFunction } from "express";
 
 import { logger } from "../../logger/index.logger";
 import { HttpError } from "../../classes/http-error.class";
 
 import * as userServices from "../../services/v1/user.services";
-
-import { AuthenticatedRequestInterface } from "../../interfaces/extended-request.interface";
 
 const signIn = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -31,4 +31,33 @@ const signIn = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { signIn };
+const getUserByUsername = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const username = `${req.params.username}@gmail.com`;
+
+    const getUserResponse = await userServices.getUserByUsername({
+      username,
+    });
+
+    return res
+      .status(getUserResponse.getStatus())
+      .json(getUserResponse.getResponse());
+  } catch (error) {
+    logger.error(
+      "[Controller: getUserByUsername] - Something went wrong",
+      error
+    );
+
+    if (error instanceof HttpError) {
+      return next(error);
+    }
+
+    return next(new HttpError(500, "Something went wrong in getting user"));
+  }
+};
+
+export { signIn, getUserByUsername };
