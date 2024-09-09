50f3779235a29f7e12350024fff745d7f9ebec43
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-09 17:50:44 +0530

    feat: Add length validation in getCommentsByPostIdValidatorSchema

--- src/validators/comment-routes.validators.ts
+++ src/validators/comment-routes.validators.ts
@@ -21,6 +21,6 @@ export const getCommentsByPostIdValidatorSchema = {
     postId: Joi.string().alphanum().length(24).required(),
   }),
   querySchema: Joi.object({
-    cursor: Joi.string().alphanum().optional(),
+    cursor: Joi.string().alphanum().length(24).optional(),
   }),
 };
