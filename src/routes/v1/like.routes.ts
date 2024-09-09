e266bd24407d6875443d384ac916e1b774eb031a
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-09 17:38:29 +0530

    feat: Add like route

---
+++ src/routes/v1/like.routes.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import * as likeController from "../../controllers/v1/like.controllers";
+import * as likeValidationSchemas from "../../validators/like-routes.validators";
+
+import { authMiddleware } from "../../middlewares/auth.middleware";
+import { userMiddleware } from "../../middlewares/user.middleware";
+import { validate } from "../../middlewares/validate.middleware";
+
+const likeRouter = express.Router();
+
+likeRouter.use(authMiddleware);
+likeRouter.use(userMiddleware);
+
+likeRouter.post(
+  "/",
+  validate(likeValidationSchemas.likePostOrCommentValidatorSchema),
+  likeController.likePostOrComment
+);
+
+export default likeRouter;
