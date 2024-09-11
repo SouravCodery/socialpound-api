5999b612359b616123c7eac3d8cab6c30f2e5117
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-11 16:58:55 +0530

    refactor: Remove incrementLikeOrCommentCount

--- src/services/v1/persistent-redis.services.ts
+++ src/services/v1/persistent-redis.services.ts
@@ -1,25 +1,6 @@
 import { persistentRedisClient } from "../../config/redis-persistent.config";
 import { logger } from "../../logger/index.logger";
 
-export const incrementLikeOrCommentCount = async ({
-  entity,
-  id,
-  countType,
-}: {
-  entity: "Post" | "Comment";
-  id: string;
-  countType: "likesCount" | "commentsCount";
-}) => {
-  try {
-    const hashKey = `${entity}:${id}:counter`;
-    const result = await persistentRedisClient.hIncrBy(hashKey, countType, 1);
-
-    return result;
-  } catch (error) {
-    logger.error("[Service: incrementCount] - Something went wrong", error);
-  }
-};
-
 export const incrementLikeOrCommentCountInBulk = async ({
   entityType,
   ids,
