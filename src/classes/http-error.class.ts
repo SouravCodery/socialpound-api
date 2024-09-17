99b9c358b5c4014c36fe4f389392a3aeae06349f
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-17 13:27:07 +0530

    feat:Add toastMessage, Change the param structure of HttpError

--- src/classes/http-error.class.ts
+++ src/classes/http-error.class.ts
@@ -1,9 +1,19 @@
 export class HttpError extends Error {
   public status: number;
+  public toastMessage: string;
 
-  constructor(status: number, message: string) {
+  constructor({
+    status,
+    message,
+    toastMessage,
+  }: {
+    status: number;
+    message: string;
+    toastMessage?: string;
+  }) {
     super(message);
     this.status = status;
+    this.toastMessage = message;
 
     Object.setPrototypeOf(this, HttpError.prototype);
   }
