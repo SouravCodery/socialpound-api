0bb0be17f08766076c3812c2ff0a010c0c94d419
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-21 02:19:15 +0530

    feat: Make cacheMiddleware HOF with authenticatedUserId param

--- src/middlewares/cache.middleware.ts
+++ src/middlewares/cache.middleware.ts
@@ -1,41 +1,48 @@
 import { Request, Response, NextFunction } from "express";
 import { getCache } from "../services/v1/redis-cache.services";
+import { getCacheKey } from "../helpers/cache.helpers";
 import { Config } from "../config/config";
 import { logger } from "../logger/index.logger";
 
-export const cacheMiddleware = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (req.method === "OPTIONS") {
-    return next();
-  }
-
-  if (Config.REDIS_CACHE_ENABLED === false) {
-    return next();
-  }
-
-  try {
-    const cacheKey = req.originalUrl;
-    const cachedData = await getCache({ key: cacheKey });
-
-    if (cachedData) {
-      logger.info(`[Cache Hit] - Cache found for ${cacheKey}`);
-
-      res.setHeader("Accelerated", "true");
-      return res.status(200).json(cachedData);
+export function cacheMiddleware({
+  authenticatedUserId,
+}: {
+  authenticatedUserId: string | null;
+}) {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    if (req.method === "OPTIONS") {
+      return next();
     }
 
-    logger.info(`[Cache Miss] - No cache found for ${cacheKey}`);
-  } catch (error) {
-    logger.error(
-      `[Middleware: cacheMiddleware] - Error fetching cache for ${req.originalUrl}`,
-      error
-    );
-  } finally {
-    logger.info("Checking!");
-  }
+    if (Config.REDIS_CACHE_ENABLED === false) {
+      return next();
+    }
 
-  next();
-};
+    try {
+      const cacheKey = getCacheKey({
+        url: req.baseUrl,
+        params: req.params,
+        query: req.query,
+        authenticatedUserId,
+      });
+
+      const cachedData = await getCache({ key: cacheKey });
+
+      if (cachedData) {
+        logger.info(`[Cache Hit] - Cache found for ${cacheKey}`);
+
+        res.setHeader("Accelerated", "true");
+        return res.status(200).json(cachedData);
+      }
+
+      logger.info(`[Cache Miss] - No cache found for ${cacheKey}`);
+    } catch (error) {
+      logger.error(
+        `[Middleware: cacheMiddleware] - Error fetching cache for ${req.originalUrl}`,
+        error
+      );
+    }
+
+    next();
+  };
+}
