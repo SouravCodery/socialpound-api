95b3f090dc399cbe8a0b1dc086746917c7563f76
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-21 02:07:03 +0530

    feat: Add getCacheKey helper

---
+++ src/helpers/cache.helpers.ts
@@ -0,0 +1,45 @@
+import { Config } from "../config/config";
+
+export const getCacheKey = ({
+  url,
+  params,
+  query,
+  authenticatedUserId,
+}: {
+  url: string;
+  params: object;
+  query: object;
+  authenticatedUserId?: string;
+}): string => {
+  const processedUrl = url.replaceAll("/", ":");
+
+  const processedParam = Object.entries(params)
+    .map(([key, value]) => `${key}:${value}`)
+    .join(":");
+
+  const processedQuery = Object.entries(query)
+    .map(([key, value]) => `${key}:${value}`)
+    .join(":");
+
+  const authenticatedUserPart = authenticatedUserId
+    ? `${authenticatedUserId}`
+    : "";
+
+  let cacheKey = `${Config.NODE_ENV}:api-cache${processedUrl}`;
+
+  if (processedParam) {
+    cacheKey += `:${processedParam}`;
+  }
+
+  if (processedQuery) {
+    cacheKey += `:${processedQuery}`;
+  } else {
+    cacheKey += `:cursor:none`;
+  }
+
+  if (authenticatedUserId) {
+    cacheKey += `:${authenticatedUserPart}`;
+  }
+
+  return cacheKey;
+};
