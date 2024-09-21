bbb758e8d584941968ad6f9520c6879d64e2690d
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-21 14:30:42 +0530

    feat: Add setAPICache in user, like, comment, notification controllers
    Controllers: getUserByUsername, getLikesByPostId, getPostsLikedByUser, getCommentsByPostId, getNotificationsByUser

--- src/controllers/v1/notification.controllers.ts
+++ src/controllers/v1/notification.controllers.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from "express";
-import { logger } from "../../logger/index.logger";
 
-import { AuthenticatedUserRequestInterface } from "../../interfaces/extended-request.interface";
-import { HttpError } from "../../classes/http-error.class";
 import * as notificationServices from "../../services/v1/notification.services";
+import { setAPICache } from "../../services/v1/redis-cache.services";
+import { logger } from "../../logger/index.logger";
+import { HttpError } from "../../classes/http-error.class";
+import { AuthenticatedUserRequestInterface } from "../../interfaces/extended-request.interface";
 
 export const addMarkNotificationAsRead = async (
   req: Request,
@@ -51,14 +52,20 @@ export const getNotificationsByUser = async (
   next: NextFunction
 ) => {
   try {
-    const recipient = (
-      req as AuthenticatedUserRequestInterface
-    ).user._id.toString();
+    const recipient = (req as AuthenticatedUserRequestInterface).userId;
     const { cursor } = req.query;
 
     const notifications = await notificationServices.getNotificationsByUser({
       recipient,
       cursor: cursor?.toString(),
+    });
+
+    await setAPICache({
+      url: req.baseUrl,
+      params: req.params,
+      query: req.query,
+      authenticatedUserId: recipient,
+      value: notifications.getResponse(),
     });
 
     return res
