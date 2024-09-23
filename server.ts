471d1665234ec61db6a6693aa48a620830b9e8f0
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-23 14:13:04 +0530

    feat: Change connectToDatabase to named export

--- server.ts
+++ server.ts
@@ -1,13 +1,12 @@
 import app from "./app";
-import connectToDatabase from "./src/config/database.config";
-import { Config } from "./src/config/config";
 
+import { connectToDatabase } from "./src/config/database.config";
+import { Config } from "./src/config/config";
 import { redisKeyValueStoreClient } from "./src/config/redis-key-value-store.config";
 import { redisCacheClient } from "./src/config/redis-cache.config";
 
-import { logger } from "./src/logger/index.logger";
-
 import "./src/mq/bull-mq/index.bull-mq";
+import { logger } from "./src/logger/index.logger";
 
 const port = Config.PORT;
 
@@ -21,7 +20,7 @@ const port = Config.PORT;
 
     logger.info("Connected to persistentRedis and cacheRedis");
 
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
       logger.info(`Socialpound API listening on: ${port}`);
     });
   } catch (error) {
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -17,4 +17,4 @@ const connectToDatabase = async () => {
   }
 };
 
-export default connectToDatabase;
+export { connectToDatabase };
