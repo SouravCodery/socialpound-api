5f9b73af94480825d8fce9b8f0db23c62994f065
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-25 18:46:05 +0530

    feat: Add morganMiddleware

---
+++ src/middlewares/morgan.middleware.ts
@@ -0,0 +1,27 @@
+import morgan, { StreamOptions, TokenIndexer } from "morgan";
+import { IncomingMessage, ServerResponse } from "http";
+import { logger } from "../logger/winston.logger";
+
+const jsonFormat = (
+  tokens: TokenIndexer<IncomingMessage, ServerResponse>,
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  return JSON.stringify({
+    method: tokens.method(req, res),
+    url: tokens.url(req, res),
+    status: tokens.status(req, res),
+    content_length: tokens.res(req, res, "content-length"),
+    response_time: tokens["response-time"](req, res) + " ms",
+    timestamp: new Date().toISOString(),
+    ip: tokens["remote-addr"](req, res),
+  });
+};
+
+const stream: StreamOptions = {
+  write: (message: string) => {
+    logger.http("server requests", JSON.parse(message));
+  },
+};
+
+export const morganMiddleware = morgan(jsonFormat, { stream });
