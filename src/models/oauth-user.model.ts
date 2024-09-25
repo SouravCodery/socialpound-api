82336b8393be35044682211639fb011514d89b09
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-26 02:08:55 +0530

    refactor: Delete OAuthUserSchema

--- src/models/oauth-user.model.ts
+++
@@ -1,6 +0,0 @@
-export const OAuthUserSchema = {
-  id: { type: String, required: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  image: { type: String, required: true },
-};
