60d5e3a27064e12acab2d7dc2d54fb3f75692a64
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-09-25 17:17:58 +0530

    feat: Add winston logger

---
+++ src/logger/winston.logger.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import fs from "fs";
+import { createLogger, format, transports } from "winston";
+
+const logsDir = path.join(process.cwd(), "logs");
+
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
+
+const consoleFormat = format.combine(
+  format.colorize({ all: true }), // Colorize the console output
+  format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), // Add timestamp
+  format.printf(({ timestamp, level, message, ...meta }) => {
+    const metaString = Object.keys(meta).length
+      ? JSON.stringify(meta, null, 2)
+      : "";
+    return `[${timestamp}] ${level}: ${message} ${metaString}`;
+  })
+);
+
+export const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+
+  transports: [
+    new transports.File({
+      filename: path.join(logsDir, "error.log"),
+      level: "error",
+    }),
+    new transports.File({ filename: path.join(logsDir, "combined.log") }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: consoleFormat,
+    })
+  );
+}
