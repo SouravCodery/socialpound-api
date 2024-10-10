05f735d057789ce353b3a7066930cba2236d826c
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-10-10 22:48:03 +0530

    feat: Change database.config for prisma

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,27 +1,26 @@
-import mongoose from "mongoose";
-import { Config } from "./config";
+import { PrismaClient } from "@prisma/client";
 import { logger } from "../logger/index.logger";
+
+const prisma = new PrismaClient();
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(Config.MONGODB_URI);
-    logger.info("MongoDB connected successfully!");
+    await prisma.$connect();
+    logger.info("PostgreSQL connected successfully!");
   } catch (error) {
     if (error instanceof Error) {
       logger.error("Something went wrong in connectToDatabase!", error.message);
     } else {
       logger.error("Something went wrong in connectToDatabase!", error);
     }
-
     process.exit(1);
   }
 };
 
 const disconnectFromDatabase = async () => {
   try {
-    await mongoose.disconnect();
-
-    logger.info("MongoDB disconnected successfully!");
+    await prisma.$disconnect();
+    logger.info("PostgreSQL disconnected successfully!");
   } catch (error) {
     if (error instanceof Error) {
       logger.error(
@@ -34,4 +33,4 @@ const disconnectFromDatabase = async () => {
   }
 };
 
-export { connectToDatabase, disconnectFromDatabase };
+export { connectToDatabase, disconnectFromDatabase, prisma };
