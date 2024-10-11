1ff4260dd28bd5569d6e3dee523f35f9ea22a4e2
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-10-11 15:17:55 +0530

    build: Run npx prisma migrate dev --name init

---
+++ prisma/migrations/20241011093852_init/migration.sql
@@ -0,0 +1,155 @@
+-- CreateEnum
+CREATE TYPE "CommentOnEnum" AS ENUM ('Post', 'Comment');
+
+-- CreateEnum
+CREATE TYPE "LikeOnEnum" AS ENUM ('Post', 'Comment');
+
+-- CreateEnum
+CREATE TYPE "NotificationType" AS ENUM ('like_on_post', 'like_on_comment', 'comment', 'reply', 'add_friend');
+
+-- CreateTable
+CREATE TABLE "comments" (
+    "id" SERIAL NOT NULL,
+    "commentOn" "CommentOnEnum" NOT NULL,
+    "post" INTEGER NOT NULL,
+    "parentCommentId" INTEGER,
+    "user" INTEGER NOT NULL,
+    "post_by" INTEGER NOT NULL,
+    "text" VARCHAR(2200) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "likes" (
+    "id" SERIAL NOT NULL,
+    "likeOn" "LikeOnEnum" NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "commentId" INTEGER,
+    "liker" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "likes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" SERIAL NOT NULL,
+    "recipient" INTEGER NOT NULL,
+    "sender" INTEGER NOT NULL,
+    "type" "NotificationType" NOT NULL,
+    "post" INTEGER,
+    "comment" INTEGER,
+    "read" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "user" INTEGER NOT NULL,
+    "caption" VARCHAR(2200) NOT NULL DEFAULT '',
+    "isUserDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "post_contents" (
+    "id" SERIAL NOT NULL,
+    "post" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "aspect_ratio" DOUBLE PRECISION NOT NULL DEFAULT 1.0,
+
+    CONSTRAINT "post_contents_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "full_name" TEXT NOT NULL DEFAULT '',
+    "sub" TEXT NOT NULL,
+    "profile_picture" TEXT NOT NULL DEFAULT '',
+    "bio" TEXT NOT NULL DEFAULT '',
+    "posts_count" INTEGER NOT NULL DEFAULT 0,
+    "followers_count" INTEGER NOT NULL DEFAULT 0,
+    "following_count" INTEGER NOT NULL DEFAULT 0,
+    "is_private" BOOLEAN NOT NULL DEFAULT true,
+    "last_login" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "comments_post_idx" ON "comments"("post");
+
+-- CreateIndex
+CREATE INDEX "comments_parentCommentId_idx" ON "comments"("parentCommentId");
+
+-- CreateIndex
+CREATE INDEX "likes_liker_idx" ON "likes"("liker");
+
+-- CreateIndex
+CREATE INDEX "notifications_recipient_read_idx" ON "notifications"("recipient", "read");
+
+-- CreateIndex
+CREATE INDEX "posts_user_idx" ON "posts"("user");
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_fkey" FOREIGN KEY ("post") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_parentCommentId_fkey" FOREIGN KEY ("parentCommentId") REFERENCES "comments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_user_fkey" FOREIGN KEY ("user") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "comments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_liker_fkey" FOREIGN KEY ("liker") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_recipient_fkey" FOREIGN KEY ("recipient") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_sender_fkey" FOREIGN KEY ("sender") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_post_fkey" FOREIGN KEY ("post") REFERENCES "posts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_comment_fkey" FOREIGN KEY ("comment") REFERENCES "comments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_user_fkey" FOREIGN KEY ("user") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_contents" ADD CONSTRAINT "post_contents_post_fkey" FOREIGN KEY ("post") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
