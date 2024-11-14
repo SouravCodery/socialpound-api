1e8b8e0a9bdc7f305e34db561197b418be2a9e6c
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-14 15:03:50 +0530

    feat: Add SocketConstants

---
+++ src/constants/socket.constants.ts
@@ -0,0 +1,10 @@
+export const SocketConstants = {
+  EVENTS: {
+    CONNECTION: "connection",
+    DISCONNECT: "disconnect",
+
+    CALL_FRIEND: "call-friend",
+    CALL_FAILED: "call-failed",
+    INCOMING_CALL: "incoming-call",
+  },
+} as const;
