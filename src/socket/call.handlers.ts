105af980109dcb6651bfd8daf165becc2ab03361
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-14 16:47:50 +0530

    feat: Add friendship check in callFriend, Add disconnect

--- src/socket/call.handlers.ts
+++ src/socket/call.handlers.ts
@@ -1,6 +1,7 @@
 import { Server, Socket } from "socket.io";
 
 import { SocketConstants } from "../constants/socket.constants";
+import { checkFriendshipStatus } from "./../services/v1/friendship.services";
 import { logger } from "../logger/winston.logger";
 
 const onlineUsers = new Map<string, string>();
@@ -14,31 +15,61 @@ export const callHandlers = ({
 }) => {
   onlineUsers.set(socket.data.userId, socket.id);
 
-  const callFriend = (data: { friendId: string }) => {
-    const { friendId } = data;
-    const { userId, user } = socket.data;
-    const { username } = user;
+  const callFriend = async (data: { friendId: string }) => {
+    try {
+      const { friendId } = data;
+      const { userId, user } = socket.data;
+      const { username } = user;
 
-    const friendSocketId = onlineUsers.get(friendId);
-
-    if (!friendSocketId) {
-      socket.emit(SocketConstants.EVENTS.CALL_FAILED, {
-        message: "Friend is not online",
+      const friendshipStatusResponse = await checkFriendshipStatus({
+        userId,
+        otherUser: friendId,
       });
-      return;
-    }
 
-    io.to(friendSocketId).emit(SocketConstants.EVENTS.INCOMING_CALL, {
-      message: `${username.split("@")[0]} is calling!`,
-    });
+      const isFriend =
+        friendshipStatusResponse.getResponse().data?.status === "accepted";
+
+      if (!isFriend) {
+        socket.emit(SocketConstants.EVENTS.CALL_FAILED, {
+          message: "You can only call your friends",
+        });
+        return;
+      }
+
+      const friendSocketId = onlineUsers.get(friendId);
+
+      if (!friendSocketId) {
+        socket.emit(SocketConstants.EVENTS.CALL_FAILED, {
+          message: "Friend is not online",
+        });
+        return;
+      }
+
+      io.to(friendSocketId).emit(SocketConstants.EVENTS.INCOMING_CALL, {
+        message: `${username.split("@")[0]} is calling!`,
+      });
+    } catch (error) {
+      logger.error(
+        "[Socket Handler: callFriend] - Something went wrong",
+        error
+      );
+    }
+  };
+
+  const disconnect = () => {
+    try {
+      logger.info(`A user disconnected: ${socket.id}`);
+
+      const { userId } = socket.data;
+      onlineUsers.delete(userId);
+    } catch (error) {
+      logger.error(
+        "[Socket Handler: disconnect] - Something went wrong",
+        error
+      );
+    }
   };
 
   socket.on(SocketConstants.EVENTS.CALL_FRIEND, callFriend);
-
-  socket.on(SocketConstants.EVENTS.DISCONNECT, () => {
-    logger.info(`A user disconnected: ${socket.id}`);
-
-    const { userId } = socket.data;
-    onlineUsers.delete(userId);
-  });
+  socket.on(SocketConstants.EVENTS.DISCONNECT, disconnect);
 };
