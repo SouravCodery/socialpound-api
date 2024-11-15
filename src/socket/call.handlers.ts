3daaefbbbb46a0c9d305993f33f515e2c20d3c67
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-15 19:14:52 +0530

    feat: Add callAnswer in callHandlers

--- src/socket/call.handlers.ts
+++ src/socket/call.handlers.ts
@@ -64,6 +64,55 @@ export const callHandlers = ({
     }
   };
 
+  const callAnswer = async (data: {
+    friendId: string;
+    answer: RTCSessionDescriptionInit;
+  }) => {
+    try {
+      const { friendId } = data;
+      const { userId, user } = socket.data;
+      const { username } = user;
+
+      const friendshipStatusResponse = await checkFriendshipStatus({
+        userId,
+        otherUser: friendId,
+      });
+
+      const isFriend =
+        friendshipStatusResponse.getResponse().data?.status === "accepted";
+
+      if (!isFriend) {
+        socket.emit(SocketConstants.EVENTS.CALL_FAILED, {
+          message: "You can only call your friends",
+        });
+        return;
+      }
+
+      const friendSocketId = onlineUsers.get(friendId);
+
+      if (!friendSocketId) {
+        socket.emit(SocketConstants.EVENTS.CALL_FAILED, {
+          message: "Friend is not online",
+        });
+        return;
+      }
+
+      io.to(friendSocketId).emit(SocketConstants.EVENTS.INCOMING_ANSWER, {
+        message: `${username.split("@")[0]} is answering!`,
+        answer: data.answer,
+      });
+    } catch (error) {
+      logger.error(
+        "[Socket Handler: answerCall] - Something went wrong",
+        error
+      );
+
+      socket.emit(SocketConstants.EVENTS.CALL_FAILED, {
+        message: "Call failed, Please try again!",
+      });
+    }
+  };
+
   const disconnect = () => {
     try {
       logger.info(`A user disconnected: ${socket.id}`);
@@ -79,5 +128,6 @@ export const callHandlers = ({
   };
 
   socket.on(SocketConstants.EVENTS.CALL_FRIEND, callFriend);
+  socket.on(SocketConstants.EVENTS.CALL_ANSWER, callAnswer);
   socket.on(SocketConstants.EVENTS.DISCONNECT, disconnect);
 };
