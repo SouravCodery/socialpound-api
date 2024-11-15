50607bee55d46f5811fce9663df88b7f0bcc635d
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-11-15 19:14:00 +0530

    feat: Add CALL_ANSWER, INCOMING_ANSWER in SocketConstants

--- src/constants/socket.constants.ts
+++ src/constants/socket.constants.ts
@@ -5,7 +5,10 @@ export const SocketConstants = {
 
     CALL_FRIEND: "call-friend",
     CALL_FAILED: "call-failed",
+    CALL_ANSWER: "call-answer",
+
     INCOMING_CALL: "incoming-call",
+    INCOMING_ANSWER: "incoming-answer",
 
     ERROR: "error",
   },
